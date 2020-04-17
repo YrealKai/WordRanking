f55a8a1d076ab495bb47793d60f5bbecc0931e31
Truan <truan.frames@gmail.com>
2020-04-17 12:17:24 +0200

    initial commit

---
+++ CountWords.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace WordRanking
+{
+    public static class StringExtension
+    {
+        public static string StripPunctuation(this string s)
+        {
+            var sb = new StringBuilder();
+            foreach (char c in s)
+            {
+                if (!char.IsPunctuation(c))
+                    sb.Append(c);
+            }
+            return sb.ToString();
+        }
+    }
+    public class CountWords
+    {
+        public Dictionary<string, int> CountWordsAndOrder(string title, int length, string order)
+        {
+            string path = @"../../../Books/";
+            string ext = "";
+            var listing = new Dictionary<string, int>();
+            var files = Directory.GetFiles(path, title + ".*");
+            if (files.Length > 0)
+            {
+                ext = files[0].Substring(files[0].Length - 3, 3);
+                
+            }
+            string pathAndTitle = path + title + "." +ext;
+            
+            foreach (string line in File.ReadLines(pathAndTitle))
+            {
+                var words = line.StripPunctuation().Split(" ");
+                foreach (var word in words)
+                {
+                    if (word.Length >= length)
+                    {
+                        if (!listing.ContainsKey(word))
+                        {
+                            listing.Add(word, 1);
+                        }
+                        else
+                        {
+                            var currentCount = listing[word];
+                            listing[word] = currentCount + 1;
+                        }
+                    }
+                }
+            }
+            if (order == "asc") { 
+                return listing.OrderBy(x => x.Value).ToDictionary(pair => pair.Key, pair => pair.Value);
+            }
+            else
+            {
+                return listing.OrderByDescending(x => x.Value).ToDictionary(pair => pair.Key, pair => pair.Value);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+
+namespace WordRanking
+{
+    class Program
+    {
+        static void Main()
+        {
+            try
+            {
+                Console.WriteLine("Please enter the book title you wish to read:");
+                var title = Console.ReadLine();
+                var length = AskLength();
+                var order = AskOrder();
+                Stopwatch timer = new Stopwatch();
+                timer.Start();
+                var cw = new CountWords();
+                var countListing = cw.CountWordsAndOrder(title, length, order);
+                Console.WriteLine("-----------------------------------------------------------------------");
+                for (int i = 0; i<=49; i++)
+                {
+                    Console.WriteLine(countListing.Keys.ElementAt(i) + " - " + countListing.Values.ElementAt(i));
+                }
+                timer.Stop();
+
+                Console.WriteLine("This process took -" + timer.Elapsed);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+
+        }
+
+        public static int AskLength()
+        {
+            Console.WriteLine("Please enter length of the words (and up) you wish to count");
+            var input = Console.ReadLine();
+            if (!int.TryParse(input, out int length))
+            {
+                AskLength();
+            }
+            return length;
+        }
+        public static string AskOrder()
+        {
+            Console.WriteLine("Please enter whether you would like to sort them in ascending (enter asc) order or decending (enter desc) order");
+            var input = Console.ReadLine();
+            if (input != "asc" && input != "desc")
+            {
+                AskOrder();
+            }
+            return input;
+        }
+    }
+}
